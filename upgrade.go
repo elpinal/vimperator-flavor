7ee1a1dbab05c7d00eb36edb8613493201fd3cad
elpinal <elpinal@users.noreply.github.com>
2016-04-10 21:32:00 +0900

    Add upgrade subcommand

--- upgrade.go
+++ upgrade.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+	"strings"
+
+	"github.com/susp/vimperator-flavor/parser"
+)
+
 var cmdUpgrade = &Command{
 	Run:       runUpgrade,
 	UsageLine: "upgrade ",
@@ -16,6 +27,66 @@ func init() {
 
 // runUpgrade executes upgrade command and return exit code.
 func runUpgrade(args []string) int {
+	repos, err := parser.ParseFile("VimperatorFlavor")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	// TODO: fix in case GOPATH is set to multiple paths
+	srcRoot := filepath.Join(os.Getenv("GOPATH"), "src")
+
+	usr, err := user.Current()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	flavorsRoot := usr.HomeDir + "/.vimperator/flavors"
+
+	for _, repo := range repos {
+		if _, err := os.Stat(srcRoot + "/" + repo.Path); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return 1
+		}
+
+		joinedPath := strings.Replace(repo.Path, "/", "_", -1)
+		flavorsDir := flavorsRoot + "/" + joinedPath
+		if _, err := os.Stat(flavorsDir); err != nil {
+			continue
+		}
+
+		rev := "master"
+		if repo.Version != "" {
+			rev = repo.Version
+		}
+
+		cmd := exec.Command("git", "pull")
+		cmd.Dir = srcRoot + "/" + repo.Path
+		/*
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return 1
+		}
+		*/
+
+		if err := os.RemoveAll(flavorsDir); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return 1
+		}
+		if err := os.Mkdir(flavorsDir, 0777); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return 1
+		}
+
+		cmd = exec.Command("sh", "-c", "git archive --prefix="+joinedPath+"/ "+rev+" | (cd "+flavorsRoot+" && tar xf -)")
+		cmd.Dir = srcRoot + "/" + repo.Path
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return 1
+		}
+	}
 
 	return 0
 }
